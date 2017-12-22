6a0f3136ae3717152de7c44ea58fc48d06140fa7
ZHAOHUHU <3066918701@qq.com>
2017-12-22 11:07:26 +0800

    删除

--- src/main/java/com/atguigu/cpes/bean/User.java
+++ src/main/java/com/atguigu/cpes/bean/User.java
@@ -1,17 +1,10 @@
 package com.atguigu.cpes.bean;
 
 /**
- *我是儿子
  */
 public class User {
 
 	private Integer id在v对付中国v发的是这个色;
-	/*
-	我是父亲
-	 */
-/*
-爱不是因为寂寞才存在，寂寞时的爱为何你要那么坏
- */
 
 
 
@@ -20,7 +13,5 @@ public class User {
 
 
 
-	/*
-	差点成孙子
-	 */
+
 }
